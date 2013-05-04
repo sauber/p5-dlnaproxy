08e675bf46cd1cc7bd329a8e910927a8d669342d
Soren Dossing <netcom@sauber.net>
2013-05-04 20:13:02 +0900

    Basic discovery with object oriented POE

--- poeloop.pl
+++ poeloop.pl
@@ -1,54 +1,67 @@
 #!/usr/bin/env perl
 
-use warnings;
-use strict;
+#use warnings;
+#use strict;
 
+########################################################################
+###
+### A SSDP Server that listens for and sends multicast packages
+###
+########################################################################
+
+package DP::SSDP;
+
+use Moose;
+use MooseX::Method::Signatures;
 use POE;
 use IO::Socket::Multicast;
 
+use constant DEBUG             => 1;
 use constant DATAGRAM_MAXLEN   => 1024;
 use constant MCAST_PORT        => 1900;
 use constant MCAST_GROUP       => '239.255.255.250';
 use constant MCAST_DESTINATION => MCAST_GROUP . ':' . MCAST_PORT;
+use constant DISCOVER_INTERVAL => 10;
+use constant DISCOVER_PACKET   => 
+'M-SEARCH * HTTP/1.1
+Host: ' . MCAST_GROUP . ':' . MCAST_PORT . '
+Man: "ssdp:discover"
+ST: upnp:rootdevice
+MX: 3
 
-POE::Session->create(
-  inline_states => {
-    _start         => \&peer_start,
-    get_datagram   => \&peer_read,
-    send_something => \&send_something,
-  }
-);
+';
 
-POE::Kernel->run();
-exit;
+has _session => ( is=>'ro', isa=>'POE::Session', lazy_build=>1 );
+method _build__session {
+  POE::Session->create(
+    object_states => [
+      $self => [ qw(_start discover read) ]
+    ]
+  ) or die $!;
+}
 
-### Set up the peer socket.
-
-sub peer_start {
-  my $kernel = $_[KERNEL];
-
-  # Don't specify an address.
-  my $socket = IO::Socket::Multicast->new(
+has _socket => ( is=>'ro', isa=>'IO::Socket::Multicast', lazy_build=>1 );
+method _build__socket {
+  IO::Socket::Multicast->new(
     LocalPort => MCAST_PORT,
     ReuseAddr => 1,
     ReusePort => 1,
   ) or die $!;
-
-  $socket->mcast_add(MCAST_GROUP) or die $!;
-
-  # Don't mcast_loopback(0). 	This disables multicast datagram
-  # delivery to all peers on the interface.  Nobody gets data.
-
-  # Begin watching for multicast datagrams.
-  $kernel->select_read($socket, "get_datagram");
-
-  # Send something once a second.  Pass the socket as a continuation.
-  $kernel->delay(send_something => 1, $socket);
 }
 
-### Receive a datagram when our sicket sees it.
+# Announce to the world that we are looking for servers
+#
+sub discover {
+  my($self, $kernel) = @_[OBJECT,KERNEL];
 
-sub peer_read {
+  warn $! unless
+    $self->_socket->mcast_send(DISCOVER_PACKET, MCAST_DESTINATION);
+
+  warn "*** pid $$ sent discover at " . time() . "\n" if DEBUG;
+  $kernel->delay(discover => DISCOVER_INTERVAL);
+}
+
+sub read {
   my ($kernel, $socket) = @_[KERNEL, ARG0];
 
   my $remote_address = recv($socket, my $message = "", DATAGRAM_MAXLEN, 0);
@@ -62,13 +75,28 @@ sub peer_read {
   print "received from $human_addr : $peer_port ... $message\n";
 }
 
-### Periodically send something.
 
-sub send_something {
-  my ($kernel, $socket) = @_[KERNEL, ARG0];
+sub _start {
+  my($self, $kernel) = @_[OBJECT, KERNEL];
 
-  my $message = "pid $$ sending at " . time() . " to " . MCAST_DESTINATION;
-  warn $! unless $socket->mcast_send($message, MCAST_DESTINATION);
+  warn "*** $self session called _start\n" if DEBUG;
 
-  $kernel->delay(send_something => 1, $socket);
+  # With the session started, tell kernel to call "read" sub
+  # when data arrives on socket.
+  $kernel->select_read($self->_socket, "read");
+
+  # Start sending out discovery packets
+  $kernel->yield('discover');
+
+  warn "*** $self session ended _start\n" if DEBUG;
 }
+
+method run {
+  $self->_session;
+
+  POE::Kernel->run();
+}
+
+__PACKAGE__->meta->make_immutable;
+
+DP::SSDP->new->run;
