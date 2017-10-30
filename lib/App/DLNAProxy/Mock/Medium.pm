ee32c58f80061fd9280d8e548bb69da97adb59dc
Søren Døssing (privat) <sauber@sd.local>
2017-10-30 18:25:01 +0100

    High level implementation of a use case

--- lib/App/DLNAProxy/Mock/Medium.pm
+++ lib/App/DLNAProxy/Mock/Medium.pm
@@ -5,10 +5,18 @@ use namespace::clean;
 
 has packets => ( is=>'ro', default=>sub {[]} );
 has discovery_packet => ( is=>'ro', default=>"M-SEARCH" );
+has reader => ( is=>'rw', default=>sub {} );
 
 sub broadcast_discovery {
   my $self = shift;
   push @{ $self->packets }, $self->discovery_packet;
+}
+
+sub read {
+  my $self = shift;
+  my $packet = shift;
+
+  $self->reader->( $packet );
 }
 
 1;
--- lib/App/DLNAProxy/Usecases.pm
+++ lib/App/DLNAProxy/Usecases.pm
@@ -7,11 +7,28 @@ has medium => ( is=>'ro', required=>1 );
 has timer  => ( is=>'ro', required=>1 );
 has discovery_interval => ( is=>'ro', default=>900 );
 
+# Discovery messages are sent regularly
+#
 sub regular_discovery {
   my $self = shift;
 
   my $callback = sub { $self->medium->broadcast_discovery };
   $self->timer->timed( $self->discovery_interval, $callback );
+}
+
+# When discovery is received, resend to all other interfaces
+#
+sub read_discovery {
+  my $self = shift;
+
+  # Closure to handle incoming packets
+  my $callback = sub {
+    my $packet = shift;
+    $self->medium->distribute( $packet );
+  };
+
+  # Register handler in medium
+  $self->medium->reader( $callback );
 }
 
 1;
